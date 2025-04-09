3ace295b74e2601f448669f8417925e3d8701509
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-04-09 16:26:07 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Avatar } from './components/Avatar';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Profile } from './components/Profile';
+
 function App() {
 	return (
 		<div className="min-h-screen bg-gray-50 text-gray-800">
-			<main className="p-4"></main>
+			<Header title={'Evening checkout'} />
+			<main className="p-4">
+				<Profile
+					name={'Godwin'}
+					title={'React dev'}
+					location={'Nairobi, Kenya'}
+					avatar={
+						'https://preview.redd.it/i-dont-remember-if-it-was-ever-mentioned-but-how-did-iroh-v0-tnt0gpu0xhy81.jpg?width=640&crop=smart&auto=webp&s=a97415b1f36a10f31bfb66a0b85d7da124b3a583'
+					}
+				/>
+				<Avatar
+					src={
+						'https://upload.wikimedia.org/wikipedia/en/1/12/Azula.png'
+					}
+					alt={'Azula'}
+				/>
+			</main>
+			<Footer message={'Copyright ©️'} />
 		</div>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-export function Header({ title }) {
+export function Header(props) {
+	const { title } = props;
+	// console.log(title);
+	// console.log(props);
+	//inline destructuring
 	return (
 		<header className="bg-blue-600 text-white p-4 shadow-md">
 			<h1 className="text-xl font-semibold">{title}</h1>
 		</header>
 	);
 }
+
+// props are values that are passed between
+// components
