1627406eb91090dbc364018467cb234867fd3111
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-04-10 13:12:41 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { Avatar } from "./components/Avatar";
-import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { Info } from "./components/Info";
 import { Profile } from "./components/Profile";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
       <main className="p-4">
-       <Header/>
-			  <Avatar />
-			  <Profile/>
-			  <Info/>
+        <Header />
+        <Profile />
         <Footer />
       </main>
     </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,13 @@
-export function Profile({ name, title, location, avatar }) {
+import { Avatar } from "./Avatar";
+import { Info } from "./Info";
+
+export function Profile() {
   return (
-    <div className="bg-white rounded-lg shadow p-6 flex gap-4 items-center">About Me</div>
+    <div className="bg-white rounded-lg shadow p-6 flex gap-4 items-center">
+      <Avatar
+        
+		  />
+		  <Info/>
+    </div>
   );
 }
